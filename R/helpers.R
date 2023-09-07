55eefed44657700ede1d390707abfd71c51aa495
Yevhenii Boiko <yevhenii.boiko@atorusresearch.com>
2023-09-07 11:12:43 +0300

    Update get_group_starts_from_df function to take a vector of indents

--- R/helpers.R
+++ R/helpers.R
@@ -49,9 +49,8 @@ get_groups_from_df <- function(df, indents=c("\t"), row_label_no=1){
     for(i in 1:(nrow(df)-1)) {
         next_indent <- FALSE
         for(j in 1:length(indents)){
-            if(is.na(indents[[j]])){ next_indent <- is.na(df[[i+1,row_label_no]]) else
-                next_indent <- grepl(paste0("^", indents[[j]]), df[[i+1,row_label_no]]) | next_indent
-            }
+            if(is.na(indents[[j]])){ next_indent <- is.na(df[[i+1,row_label_no]])}
+            else next_indent <- grepl(paste0("^", indents[[j]]), df[[i+1,row_label_no]]) | next_indent
         }
         if (next_indent){groups_result <- c(groups_result, i)}
     }
@@ -61,14 +60,37 @@ get_groups_from_df <- function(df, indents=c("\t"), row_label_no=1){
 
 #' The function to get all group-starting rows from rowlabel column
 #'
+#' Row is considered a group start if one of the specified indents is detected
+#' at its beginning, and the previous row has a different indent.
+#' Currently, group start at first row can not be detected by this function.
+#'
 #' @param df Dataframe under analysis.
 #' @param row_label_no Order number of rowlabel column.
-#'
+#' @param group_starts a list of unique indents that are used in supplied data frame
 #' @return Vector of the strings that are group-starting rows from rowlabel column
 #' @export
-get_group_starts_from_df <- function(df, row_label_no=1){
-  return (sapply(df[[row_label_no]],
-                 function(x) !grepl("\t", x, fixed=TRUE)) %>%
-            unname() %>%
-            which())
+#' @examples
+#' t_1_1 <- data.frame(list(c('Age', '\t<18', '\t18<=65', 'Weight', '\t<150', '\t150<=250')))
+#' group_starts <- get_group_starts_from_df(t_1_1, row_label_no=1)
+get_group_starts_from_df <- function(df, group_starts=c('\t'), row_label_no=1)
+    {
+
+    groups_result <- c()
+
+    detect_indent <- function(x, indent){
+        if(is.na(indent)) curr_indent <- is.na(x) else curr_indent <- grepl(paste0("^", indent), x)
+        return (curr_indent)
+        }
+
+    for(i in 2:(nrow(df)-1)) {
+        is_new_group_start <- FALSE
+        for(j in 1:length(group_starts)){
+            if(detect_indent(df[[i,row_label_no]], group_starts[[j]]) & !detect_indent(df[[i-1,row_label_no]], group_starts[[j]])) {
+                is_new_group_start = TRUE
+            }
+        }
+        if (is_new_group_start){groups_result <- c(groups_result, i)}
+    }
+    return (groups_result)
 }
+
