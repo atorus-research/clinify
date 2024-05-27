b4f6fe93205d611ac961a5f34758501ba045d47d
mike.stackhouse <Mike.Stackhouse@atorusresearch.com>
2024-05-27 18:44:57 +0000

    Incorporate paging into print method

--- R/print.R
+++ R/print.R
@@ -4,16 +4,58 @@
 #' and
 #'
 #' @param x a clintable object
-#' @param n number of pages within the clintable to print
-#' @param ... further arguments to be passed to or from other methods. They are ignored in this function.
+#' @param n number of pages within the clintable to print. Only used when
+#'   pagination is configured
+#' @param nrows number of rows to print. Only used when rows aren't configured
+#'   within the pagination method
+#' @param ... Additional parameters passed to flextable print method
 #'
 #' @return Invisible
 #' @export
 #'
-print.clintable <- function(x, n=3, ...) {
-    # TODO: Update method to slice and print first n pages
-    x <- flextable::htmltools_value(x = x)
-    x[[3]] <- gsub("(?<!th)  ", "&nbsp; ", x[[3]], perl=TRUE)
-    x[[3]] <- gsub('(<span\\b[^>]*>) ', '\\1&nbsp;', x[[3]], perl=TRUE)
-    htmltools::browsable(x)
+#' @examples
+#'
+#' ct <- clintable(mtcars)
+#'
+#' print(ct)
+#'
+#' ct <- clin_alt_pages(
+#'   ct,
+#'   key_cols = c('mpg', 'cyl', 'hp'),
+#'   col_groups = list(
+#'     c('disp', 'drat', 'wt'),
+#'     c('qsec', 'vs', 'am'),
+#'     c('gear', 'carb')
+#'   ),
+#'   max_rows = 10
+#' )
+#'
+#' print(ct)
+#'
+print.clintable <- function(x, n=3, nrows = 15, ...) {
+  refdat <- x$body$dataset
+
+  if (get_pagination_method(x) == "default") {
+    nrows <- max(c(nrows, nrow(refdat)))
+    pg <- slice_clintable(ct, 1:nrows, eval_select(x$col_keys, refdat))
+    print_clinpage(pg)
+  }
+
+  # TODO: Write the alternating page object print method
+}
+
+#' Print a clinpage object
+#'
+#' @param x A clinpage object
+#'
+#' @return Invisible
+#' @export
+#'
+#' @examples
+#'
+print_clinpage <- function(x) {
+  x <- flextable::htmltools_value(x = x)
+  x[[3]] <- gsub("(?<!th)  ", "&nbsp; ", x[[3]], perl=TRUE)
+  x[[3]] <- gsub('(<span\\b[^>]*>) ', '\\1&nbsp;', x[[3]], perl=TRUE)
+  htmltools::browsable(x)
 }
